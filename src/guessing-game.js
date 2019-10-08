883c2e84db16950d3e5d4099ffdae9ee7055eac4
AliaksandrLytsin <55144672+AliaksandrLytsin@users.noreply.github.com>
2019-10-08 13:47:55 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.guessNumber = this.min + Math.ceil((this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
-    }
+        this.min = this.guessNumber;
+    }    
 }
 
 module.exports = GuessingGame;
